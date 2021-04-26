9539609f6ee83cceac78c85502eeda080a53ae84
Jony Reis <jonyreiscardoso@gmail.com>
2021-04-26 13:20:08 -0400

    rename tracking

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const instance = createInstance({
   disabled: process.env.NODE_ENV === 'development',
   urlBase: matomoUrl,
   siteId: 1,
-  trackerUrl: `${matomoUrl}/api.php`,
-  srcUrl: `${matomoUrl}/api.js`,
+  trackerUrl: `${matomoUrl}/tracking.php`,
+  srcUrl: `${matomoUrl}/tracking.js`,
   linkTracking: false,
   configurations: {
     setSecureCookie: true,
