48dcccd5b9e26b244c399ae2ea9ab4eb75355dc9
Jony <jonyreiscardoso@gmail.com>
2021-05-04 23:07:02 -0400

    remove matomo track react

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
+// import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
 import App from './App';
 
-const matomoUrl = 'https://suajornadanameditacao.matomo.cloud';
+// const matomoUrl = 'https://suajornadanameditacao.matomo.cloud';
 
-const instance = createInstance({
-  disabled: process.env.NODE_ENV === 'development',
-  urlBase: matomoUrl,
-  siteId: 1,
-  trackerUrl: `${matomoUrl}/tracking.php`,
-  srcUrl: `${matomoUrl}/tracking.js`,
-  linkTracking: false,
-  configurations: {
-    setSecureCookie: true,
-    setRequestMethod: 'POST'
-  }
-})
+// const instance = createInstance({
+//   disabled: process.env.NODE_ENV === 'development',
+//   urlBase: matomoUrl,
+//   siteId: 1,
+//   trackerUrl: `${matomoUrl}/tracking.php`,
+//   srcUrl: `${matomoUrl}/tracking.js`,
+//   linkTracking: false,
+//   configurations: {
+//     setSecureCookie: true,
+//     setRequestMethod: 'POST'
+//   }
+// })
 
 ReactDOM.render(
-  <MatomoProvider value={instance}>
+  // <MatomoProvider value={instance}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </MatomoProvider>,
+    </React.StrictMode>,
+  // </MatomoProvider>
   document.getElementById('root')
 );
